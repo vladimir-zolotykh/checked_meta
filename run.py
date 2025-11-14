362aca3b33179b5271898e42fb047cb3a9e6bcae
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-14 08:40:51 +0200

    Make step one (get_type_hints)
        print(get_type_hints(Movie))
    	. . .
        print(movie.__annotations__)

--- run.py
+++ run.py
@@ -1,7 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import get_type_hints
+
+
+class CheckedMeta(type):
+    def __new__(mcls, clsname, bases, clsdict):
+        return super().__new__(mcls, clsname, bases, clsdict)
+
+
+class Checked:
+    pass
+
+
+class Movie(Checked):
+    def __init__(self, title, year, box_office):
+        self.title = title
+        self.year = year
+        self.box_office = box_office
+
+    title: str
+    year: int
+    box_office: float
 
 
 if __name__ == "__main__":
-    pass
+    print(get_type_hints(Movie))
+    movie = Movie(title="The Godfather", year=1972, box_office=137)
+    # movie = Movie()
+    print(movie.__annotations__)
