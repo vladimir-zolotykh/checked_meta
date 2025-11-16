e7bff062728f90d10fb40494c6b0a6b826704244
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-11-16 21:13:43 +0200

    Add test_assignment_attribute_error() test

--- test_checked.py
+++ test_checked.py
@@ -38,3 +38,15 @@ def test_constructor_attribute_error():
         felix = Cat(name="Felix", weight=3.2, age=7)  # noqa F481
 
     assert str(e.value) == "got an unexpected keyword argument 'age'"
+
+
+def test_assignment_attribute_error():
+    class Cat(Checked):
+        name: str
+        weight: float
+
+    felix = Cat(name="Felix", weight=3.2)
+    with pytest.raises(AttributeError) as e:
+        felix.color = "tan"
+
+    assert str(e.value) == "'Cat' object has no attribute 'color'"
